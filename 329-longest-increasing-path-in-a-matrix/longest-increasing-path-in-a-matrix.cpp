8726713a45f7dcf84c48366dffbef94511b4a07e
RoshanKarthik <152865040+RoshanKarthik@users.noreply.github.com>
2026-08-30 12:39:01 +0530

    Time: 23 ms (23.35%) | Memory: 22.4 MB (27.80%) - LeetSync

--- 329-longest-increasing-path-in-a-matrix/longest-increasing-path-in-a-matrix.cpp
+++ 329-longest-increasing-path-in-a-matrix/longest-increasing-path-in-a-matrix.cpp
@@ -1,31 +1,53 @@
 class Solution {
-private:
-    int dp[205][205];
-    int n,m;
-    int drow[4] = {1,0,-1,0};
-    int dcol[4] = {0,1,0,-1};
-    int rec(int i, int j, vector<vector<int>>& matrix){
-        if(dp[i][j] != -1) return dp[i][j];
-        dp[i][j] = 1;
-        for(int d=0; d<4; d++){
-            int ni = drow[d] + i;
-            int nj = dcol[d] + j;
-            if(ni < 0 || nj < 0 || ni >= n || nj >= m || matrix[i][j] >= matrix[ni][nj]) continue;
-            dp[i][j] = max(dp[i][j], 1+rec(ni,nj,matrix));
-        }
-        return dp[i][j];
-    }
 public:
     int longestIncreasingPath(vector<vector<int>>& matrix) {
-        n = matrix.size();
-        m = matrix[0].size();
-        memset(dp,-1,sizeof(dp));
-        int ans = 1;
+        int n = matrix.size();
+        int m = matrix[0].size();
+        vector<vector<int>>indegree(n, vector<int>(m, 0));
+        int drow[] = {0,-1,0,1};
+        int dcol[] = {1,0,-1,0};
         for(int i=0; i<n; i++){
             for(int j=0; j<m; j++){
-                ans = max(ans,rec(i,j,matrix));
+                for(int d=0; d<4; d++){
+                    int ni = drow[d] + i;
+                    int nj = dcol[d] + j;
+                    if(ni >= 0 && nj >= 0 && ni < n && nj < m && matrix[i][j] < matrix[ni][nj]){
+                        indegree[ni][nj]++;
+                    }
+                }
             }
         }
+
+        queue<pair<int,int>>q;
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(indegree[i][j] == 0){
+                    q.push({i,j});
+                }
+            }
+        }
+
+        int ans = 0;
+        while(!q.empty()){
+            int size = q.size();
+            ans++;
+            while(size--){
+                int r = q.front().first;
+                int c = q.front().second;
+                q.pop();
+                for(int d=0; d<4; d++){
+                    int nr = drow[d] + r;
+                    int nc = dcol[d] + c;
+                    if(nr >= 0 && nc >= 0 && nr < n && nc < m && matrix[r][c] < matrix[nr][nc]){
+                        indegree[nr][nc]--;
+                        if(indegree[nr][nc] == 0){
+                            q.push({nr,nc});
+                        }
+                    } 
+                }
+            }
+        }
+
         return ans;
     }
 };
