21219e18950362efe30e2ad6d6e8899a62a83b94
RoshanKarthik <152865040+RoshanKarthik@users.noreply.github.com>
2026-09-04 08:22:21 +0530

    Time: 16 ms (5.31%) | Memory: 31.2 MB (56.73%) - LeetSync

--- 4284-smallest-stable-index-i/smallest-stable-index-i.cpp
+++ 4284-smallest-stable-index-i/smallest-stable-index-i.cpp
@@ -2,18 +2,16 @@ class Solution {
 public:
     int firstStableIndex(vector<int>& nums, int k) {
         int n = nums.size();
-        vector<int>a(n),b(n);
-
-        a[0] = nums[0];
-        for(int i=1; i<n; i++){
-            a[i] = max(nums[i],a[i-1]);
-        }
+        vector<int>b(n);
         b[n-1] = nums[n-1];
         for(int i=n-2; i>=0; i--){
             b[i] = min(b[i+1],nums[i]);
         }
+        int maxi = INT_MIN;
         for(int i=0; i<n; i++){
-            int score = a[i]-b[i];
+            maxi = max(maxi,nums[i]);
+            cout << maxi << " ";
+            int score = maxi-b[i];
             if(score <= k) return i;
         }
         return -1;
