3676fc3d804079341593b5bbf322320f761699c0
RoshanKarthik <152865040+RoshanKarthik@users.noreply.github.com>
2026-09-11 06:07:50 +0530

    Time: 0 ms (100.00%) | Memory: 23.8 MB (70.07%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.cpp
+++ 229-majority-element-ii/majority-element-ii.cpp
@@ -1,17 +1,37 @@
+#define pb push_back
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
         int n = nums.size();
-        unordered_map<int,int>mpp;
         vector<int>ans;
-        int mini = floor((double)n/(double)3)+1;
+        int cnt1=0,cnt2=0;
+        int el1=INT_MIN,el2=INT_MIN;
         for(int i=0; i<n; i++){
-            mpp[nums[i]]++;
-            if(mpp[nums[i]] == mini){
-                ans.push_back(nums[i]);
+            if(cnt1 == 0 && nums[i]!=el2){
+                cnt1 = 1;
+                el1 = nums[i];
             }
-            if(ans.size() == 2) break;
+            else if(cnt2 == 0 && nums[i] != el1){
+                cnt2 = 1;
+                el2 = nums[i];
+            }
+            else if(nums[i] == el1) cnt1++;
+            else if(nums[i] == el2) cnt2++;
+            else{
+                cnt1--;
+                cnt2--;
+            }
         }
+
+        cnt1=0,cnt2=0;
+        for(int i=0; i<n; i++){
+            if(nums[i] == el1) cnt1++;
+            if(nums[i] == el2) cnt2++;
+        }
+        int mini = n/3;
+        if(cnt1 > mini) ans.pb(el1);
+        if(cnt2 > mini) ans.pb(el2);
+        sort(ans.begin(),ans.end());
         return ans;
     }
 };
