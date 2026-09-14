5fc2170498f80ecccca1a220dc6a860c52416e9b
RoshanKarthik <152865040+RoshanKarthik@users.noreply.github.com>
2026-09-14 15:28:18 +0530

    Time: 601 ms (22.21%) | Memory: 40.5 MB (24.29%) - LeetSync

---
+++ 204-count-primes/count-primes.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int countPrimes(int n) {
+        if (n <= 2) return 0;
+
+        vector<bool> is_prime(n, true);
+        is_prime[0] = is_prime[1] = false;
+
+        for (int i = 3; i * i < n; i+=2) {
+            if (is_prime[i]) {
+                for (int j = i * i; j < n; j += 2*i) {
+                    is_prime[j] = false;
+                }
+            }
+        }
+
+        int ans = 1;
+        for (int i = 3; i < n; i+=2) {
+            if (is_prime[i])
+                ans++;
+        }
+
+        return ans;
+    }
+};
