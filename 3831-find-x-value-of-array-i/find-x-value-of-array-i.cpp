6e5b07d15299ad2ca1845fec8fc940844af3936b
RoshanKarthik <152865040+RoshanKarthik@users.noreply.github.com>
2026-09-21 19:27:50 +0530

    Time: 166 ms (42.86%) | Memory: 151 MB (81.25%) - LeetSync

--- 3831-find-x-value-of-array-i/find-x-value-of-array-i.cpp
+++ 3831-find-x-value-of-array-i/find-x-value-of-array-i.cpp
@@ -1,22 +1,21 @@
-int freq[5], freq2[5];
+using ll = long long;
 class Solution {
 public:
-    static vector<long long> resultArray(vector<int>& nums, int k) {
-        const int n=nums.size();
-        if (k==1) return {1LL*n*(n+1)/2};// special case
-        vector<long long> ans(k, 0);
-        memset(freq, 0, sizeof(int)*k);// freq[r]=how many times seen for x%k
-        for (int x: nums){
-            const int r=x%k;
-            memset(freq2, 0, sizeof(int)*k);
-            ans[r]++;
-            for (int j=0; j<k; j++){
-                const int prod=j*r%k;
-                freq2[prod]+=freq[j];
-                ans[prod]+=freq[j];
+    vector<long long> resultArray(vector<int>& nums, int k) {
+        int n = nums.size();
+        vector<ll>ans(k,0);
+        vector<ll>prevCount(k,0);
+        for(int i=0; i<n; i++){
+            vector<ll>currCount(k,0);
+            currCount[nums[i] % k]++;
+            for(int oldRem=0; oldRem<=k-1; oldRem++){
+                int newRem = ((ll)oldRem * nums[i]%k)%k;
+                currCount[newRem] += prevCount[oldRem];
             }
-            freq2[r]++;
-            memcpy(freq,freq2, sizeof(int)*k);
+            prevCount = currCount;
+            for(int x=0; x<=k-1; x++){
+                ans[x] += prevCount[x];
+            }
         }
         return ans;
     }
